857f49eb42305be8a09ae8c98e2fdd645885cef9
subin <subinfirow@gmail.com>
2023-04-13 16:20:03 +0530

    CRUD operations

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,46 +1,107 @@
-import * as React from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Container, Grid, Card, CardContent, Input, Button, AppBar, Toolbar, Typography } from '@mui/material';
-import { useFormik } from 'formik';
-import * as yup from 'yup';
-import axios from 'axios';
+import * as React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import {
+  Container,
+  Grid,
+  Card,
+  CardContent,
+  Input,
+  Button,
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+} from "@mui/material";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import axios from "axios";
+import { DataGrid } from "@mui/x-data-grid";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { useEffect } from "react";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#6a1b9a',
+      main: "#6a1b9a",
     },
     background: {
-      default: '#ffffff',
+      default: "#ffffff",
     },
   },
 });
 
 const validationSchema = yup.object({
-  website: yup.string().url('Please enter a valid website link').required('This field is required'),
+  website: yup
+    .string()
+    .url("Please enter a valid website link")
+    .required("This field is required"),
 });
 
 const CardGrid = () => {
   const [wordCount, setWordCount] = React.useState(0);
+  const [websites, setWebSites] = React.useState([]);
 
+  useEffect(()=>{
+    loadAll()
+  },[])
+  const loadAll = async () => {
+    try {
+        const response = await axios.get(`http://localhost:3009/websites`, {
+        });
+       
+        setWebSites(response.data)
+      } catch (error) {
+        console.error(error);
+      }
+  }
   const formik = useFormik({
     initialValues: {
-      website: '',
+      website: "",
     },
     validationSchema: validationSchema,
     onSubmit: async (values) => {
-        try {
-            const response = await axios.post('http://localhost:3009/websites', {
-              url: values.website
-            });
-            setWordCount(response.data.wordCount);
-            console.log(response.data); // handle the response data as needed
-          } catch (error) {
-            console.error(error); // handle the error as needed
-          }
+      try {
+        const response = await axios.post(`http://localhost:3009/websites`, {
+          url: values.website,
+        });
+
+        setWordCount(response.data.wordCount);
+        loadAll()
+      } catch (error) {
+        console.error(error);
+      }
     },
   });
 
+  const handleDelete = async (values) => {
+    try {
+      await axios.delete(`http://localhost:3009/websites/${values.id}`, {
+      });
+      loadAll()
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 100 },
+    { field: "url", headerName: "Website", width: 450 },
+    { field: "wordCount", headerName: "Web Count", width: 100 },
+    { field: "createdAt", headerName: "Created At", width: 100 },
+    {
+      field: "delete",
+      headerName: "Delete",
+      width: 100,
+      renderCell: (params) => (
+        <IconButton
+          aria-label="delete"
+          onClick={() => handleDelete(params.row)}
+        >
+          <DeleteIcon />
+        </IconButton>
+      ),
+    },
+  ];
   return (
     <ThemeProvider theme={theme}>
       <AppBar position="static">
@@ -50,10 +111,18 @@ const CardGrid = () => {
           </Typography>
         </Toolbar>
       </AppBar>
-      <Container maxWidth="md" sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+      <Container
+        maxWidth="md"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+        }}
+      >
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6}>
-            <Card sx={{ backgroundColor: '#f3e5f5' }}>
+            <Card sx={{ backgroundColor: "#f3e5f5" }}>
               <CardContent>
                 <h2>Insert Website Link</h2>
                 <form onSubmit={formik.handleSubmit}>
@@ -65,12 +134,21 @@ const CardGrid = () => {
                     value={formik.values.website}
                     onChange={formik.handleChange}
                     onBlur={formik.handleBlur}
-                    error={formik.touched.website && Boolean(formik.errors.website)}
+                    error={
+                      formik.touched.website && Boolean(formik.errors.website)
+                    }
                   />
                   {formik.touched.website && formik.errors.website ? (
-                    <Typography sx={{ color: 'red' }}>{formik.errors.website}</Typography>
+                    <Typography sx={{ color: "red" }}>
+                      {formik.errors.website}
+                    </Typography>
                   ) : null}
-                  <Button type="submit" variant="contained" color="primary" sx={{ marginTop: '1rem' }}>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    sx={{ marginTop: "1rem" }}
+                  >
                     Submit
                   </Button>
                 </form>
@@ -78,10 +156,25 @@ const CardGrid = () => {
             </Card>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <Card sx={{ backgroundColor: '#f3e5f5' }}>
+            <Card sx={{ backgroundColor: "#f3e5f5" }}>
               <CardContent>
                 <h2>Word Count</h2>
                 <Typography>Word Count: {wordCount}</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={12}>
+            <Card sx={{ backgroundColor: "#f3e5f5" }}>
+              <CardContent>
+                <h2>Table Data</h2>
+                <div style={{ height: 400, width: "100%" }}>
+                  <DataGrid
+                    rows={websites}
+                    columns={columns}
+                    pageSize={5}
+                    rowsPerPageOptions={[5, 10, 20]}
+                  />
+                </div>
               </CardContent>
             </Card>
           </Grid>
